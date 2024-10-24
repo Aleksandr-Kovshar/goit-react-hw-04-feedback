5391199b5bc8fd4a1155c208e2e784f18d2ab06d
Oleksandr <aleksandr.mpro@gmail.com>
2024-10-24 21:49:12 +0300

    done

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,59 +1,75 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Section from "./Section";
 import FeedbackOptions from "./FeedbackOptions";
 import Statistics from "./Statistics";
 
-class Feedback extends Component {
-  state = {
+const Feedback = () => {
+  // state = {
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0,
+  // };
+
+  const [stateFeedback, setStateFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  // handleAddFeedback = (option) => {
+  //   console.log(option);
+
+  //   this.setState((prevState) => ({
+  //     [option]: prevState[option] + 1,
+  //   }));
+  // };
+
+  const handleAddFeedback = (e) => {
+    console.log(e);
+
+    setStateFeedback((prevState) => ({ ...prevState, [e]: prevState[e] + 1 }));
+        
+
+    // this.setState((prevState) => ({
+    //   [option]: prevState[option] + 1,
+    // }));
   };
 
-  handleAddFeedback = (option) => {
-    console.log(option);
-
-    this.setState((prevState) => ({
-      [option]: prevState[option] + 1,
-    }));
-  };
-
-  countTotalFeedback = ({ good, neutral, bad }) => {
+  const countTotalFeedback = ({ good, neutral, bad }) => {
     return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage = ({ good, neutral, bad }) => {
-    const totalFeedback = this.countTotalFeedback({ good, neutral, bad });
+  const countPositiveFeedbackPercentage = ({ good, neutral, bad }) => {
+    const totalFeedback = countTotalFeedback({ good, neutral, bad });
     return totalFeedback === 0
       ? 0
       : Math.round(((good + neutral) / totalFeedback) * 100);
   };
 
-  render() {
-    const options = Object.keys(this.state);
+  const options = Object.keys(stateFeedback);
+  // const options = Object.keys(this.state);
 
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={options}
-            onLeaveFeedback={this.handleAddFeedback}
-          />
-        </Section>
-        <Section title="Statistics">
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.countTotalFeedback(this.state)}
-            positivePercentage={this.countPositiveFeedbackPercentage(
-              this.state
-            )}
-          />
-        </Section>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={handleAddFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        <Statistics
+          good={stateFeedback.good}
+          neutral={stateFeedback.neutral}
+          bad={stateFeedback.bad}
+          total={countTotalFeedback(stateFeedback)}
+          positivePercentage={countPositiveFeedbackPercentage(
+            stateFeedback
+          )}
+        />
+      </Section>
+    </>
+  );
+};
 
 export default Feedback;
